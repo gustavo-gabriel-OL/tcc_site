f28ddd37d08c2a0c57eeb4b1daec67522856f08c
gustavo-gabriel-OL <gustavogbriel2004@gmail.com>
2022-08-25 09:05:43 -0300

    25.08

--- src/usuario/controle/login-usuario.js
+++ src/usuario/controle/login-usuario.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $('.btn-login').click(function(e) {
+//     $('.btn-login').click(function(e) {
 
-        e.preventDefault();
+//         e.preventDefault();
 
-        let dados = $('#form-login').serialize()
+//         let dados = $('#form-login').serialize()
 
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            assync: true,
-            data: dados,
-            url: 'src/usuario/modelo/login-usuario.php',
-            success: function(dados) {
+//         $.ajax({
+//             type: 'POST',
+//             dataType: 'json',
+//             assync: true,
+//             data: dados,
+//             url: 'src/usuario/modelo/login-usuario.php',
+//             success: function(dados) {
 
-                if (dados.tipo == 'success') {
-                    $(location).attr('href', 'sistema.html')
-                } else {
-                    Swal.fire({
-                        title: 'FacilitaBus',
-                        text: dados.mensagem,
-                        icon: dados.tipo,
-                        confirmButtonText: 'OK'
-                    })
+//                 if (dados.tipo == 'success') {
+//                     $(location).attr('href', 'sistema.html')
+//                 } else {
+//                     Swal.fire({
+//                         title: 'FacilitaBus',
+//                         text: dados.mensagem,
+//                         icon: dados.tipo,
+//                         confirmButtonText: 'OK'
+//                     })
 
-                }
-            }
-        })
-    })
+//                 }
+//             }
+//         })
+//     })
 
-})
+// })
