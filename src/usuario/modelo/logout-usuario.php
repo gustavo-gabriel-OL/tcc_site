b75bb738ec2082c9b9ffae10f7c4f9d06b5f8810
gustavo-gabriel-Ol <gustavogbriel2004@gmail.com>
2022-09-22 08:21:36 -0300

    22/09

--- src/usuario/modelo/logout-usuario.php
+++ src/usuario/modelo/logout-usuario.php
@@ -1,12 +1,12 @@
 <?php
 
-//     session_start();
+    session_start();
 
-//     session_destroy();
+    session_destroy();
 
-//    $dados = array(
-//      'tipo' => 'success',
-//      'mensagem' => 'Sessão encerrada.'
-//   );
+   $dados = array(
+     'tipo' => 'success',
+     'mensagem' => 'Sessão encerrada.'
+  );
 
-//   echo json_encode($dados);
+  echo json_encode($dados);
